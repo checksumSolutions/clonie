3f3cc0f426b0e139b646a0aab566a9b11a9eb070
Matthew Faltys <mfaltys@gmail.com>
2014-09-15 20:38:03 -0500

    initial commit

---
+++ clonieDebDependencies.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	system("sudo apt-get update && apt-get install dd ddrescue pv");
+return 0;
+}
---
+++ clonieFedoraDependencies.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	system("sudo yum install dd ddrescue pv");
+return 0;
+}
